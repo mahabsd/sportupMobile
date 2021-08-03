31adc2d51b599e6e58dd85a1e87c2dca6681b136
saiefsmairi <saieftaher@gmail.com>
2021-08-03 20:15:06 +0100

    like and dislike

--- src/app/shared/kids/Service/postKids.service.ts
+++ src/app/shared/kids/Service/postKids.service.ts
@@ -19,9 +19,9 @@ export class PostKidsService {
     return this.utilsService.get(`${UtilsService.apiPost}?type=kids`).pipe(map(res => res.data));
   }
   likePostKids(post: Post): Observable<Post> {
-    return this.utilsService.patch(UtilsService.apiPost + 'like', post).pipe(map(res => res));
+    return this.utilsService.patch(UtilsService.apiPost + 'likePost', post).pipe(map(res => res));
   }
   disLikePostKids(post: Post): Observable<Post> {
-    return this.utilsService.patch(UtilsService.apiPost + 'dislike', post).pipe(map(res => res));
+    return this.utilsService.patch(UtilsService.apiPost + 'dislikePost', post).pipe(map(res => res));
   }
 }
