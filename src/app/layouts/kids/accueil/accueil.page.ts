2f8b933de94b68bf594d82336392e0ddbed36368
saiefsmairi <saieftaher@gmail.com>
2021-08-03 11:09:49 +0100

    posts cards +like

--- src/app/layouts/kids/accueil/accueil.page.ts
+++ src/app/layouts/kids/accueil/accueil.page.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Post } from 'src/app/shared/Model/Post';
 import { PostService } from 'src/app/shared/Service/post.service';
+/* eslint-disable @typescript-eslint/dot-notation */
+import { Component, OnInit } from '@angular/core';
+import { PostKidsService } from 'src/app/shared/kids/Service/postKids.service';
+import { Post } from 'src/app/shared/Model/Post';
+
+
 
 @Component({
   selector: 'app-accueil',
@@ -10,18 +14,26 @@ import { PostService } from 'src/app/shared/Service/post.service';
 export class AccueilPage implements OnInit {
 
   posts :any[] = [];
-  constructor(private postService: PostService) { }
+  constructor(private postKidsService: PostKidsService,private postService: PostService) { }
 
   ngOnInit() {
 this.getAllPostsKids();
   }
 
   getAllPostsKids() {
-    this.postService.getAllPostsKids().subscribe(res => {
+    this.postKidsService.getAllPostsKids().subscribe(res => {
   this.posts=res['data'];
   console.log(res['data']);
 
 
+    });
+  }
+
+  addLike(post) {
+
+    console.log('like');
+    this.postKidsService.likePostKids(post).subscribe(res => {
+       console.log(res);
     });
   }
 
