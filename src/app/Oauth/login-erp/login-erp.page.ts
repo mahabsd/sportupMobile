de0e03f4aa0aa263b0ee62ee85d94f0ccd4d7789
Achref <achref.yak@gmail.com>
2021-08-04 16:32:03 +0100

    erp login

--- src/app/Oauth/login-erp/login-erp.page.ts
+++ src/app/Oauth/login-erp/login-erp.page.ts
@@ -1,33 +1,50 @@
 import { AuthService } from './../../shared/Auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/shared/Model/User';
-import { ToastController } from '@ionic/angular';
-
+import { ToastController } from '@ionic/angular'; 
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PatternValidator } from 'src/app/shared/patternValidator';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-erp',
   templateUrl: './login-erp.page.html',
   styleUrls: ['./login-erp.page.scss'],
 })
-export class LoginErpPage implements OnInit {
-user:User=new User()
+export class LoginErpPage implements OnInit { 
+  user: User = new User();
+  loginForm: FormGroup;
   constructor(private authService: AuthService,
-    private toastCtrl:ToastController
+    private toastCtrl: ToastController,
+    private router: Router,
   ) { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      emailControl: new FormControl('', [Validators.required,
+      // eslint-disable-next-line max-len
+      PatternValidator(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+      passwordControl: new FormControl('', Validators.required)
+    });
+
   }
   login() {
     // console.log(this.user);
 
     this.authService.login(this.user).subscribe((response) => {
       // console.log('hello user', response);
-// 1 response.role ==="admin"
+      // 1 response.role ==="admin"
       // 2 navigate to ('erp')
       // else presentToast('vous navez pas le droit daccess', 'danger', 'top')
       // navigate to /tabs/home
-      location.href = '/tabs/home';
+
+      if (response.user.role === 'admin') {
+        this.router.navigateByUrl('/erp');
+      }
+      else {
+        this.router.navigateByUrl('/tabs/home');
+      }
     }, error => {
-      console.error(error);
+ 
 
     }
 
