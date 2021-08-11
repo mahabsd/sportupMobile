9f061162ead3d555a285ef34819a122e618a1016
Achref <achref.yak@gmail.com>
2021-08-11 10:21:03 +0100

    planning cal

--- src/app/layouts/planning/planning.page.ts
+++ src/app/layouts/planning/planning.page.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CalendarModalPage } from '../Planing/calendar-modal/calendar-modal.page';
+import { CalendarComponent } from 'ionic2-calendar/calendar';
 @Component({
   selector: 'app-planning',
   templateUrl: './planning.page.html',
   styleUrls: ['./planning.page.scss'],
 })
 export class PlanningPage implements OnInit {
+  calendar = {
+    mode: 'month',
+    locale:  'en-GB',
+    currentDate: new Date(),
+  };
 
+  @ViewChild(CalendarModalPage) myCal: CalendarComponent;
   constructor() { }
 
   ngOnInit() {
