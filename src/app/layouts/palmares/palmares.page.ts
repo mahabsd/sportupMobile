be719bf3b31471d973ce3b47094c3a82a6a55e43
Achref <achref.yak@gmail.com>
2021-08-31 11:11:12 +0100

    palmares modifier

--- src/app/layouts/palmares/palmares.page.ts
+++ src/app/layouts/palmares/palmares.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { User } from 'src/app/Shared/Model/User';
 import { UserService } from 'src/app/Shared/Service/user.service';
@@ -9,12 +9,17 @@ import { UserService } from 'src/app/Shared/Service/user.service';
 })
 export class PalmaresPage implements OnInit {
   // eslint-disable-next-line max-len
-  coachInfo = { name: 'Nom', lastname: 'Prenom', backgroundImage: 'https://www.nouvelleviepro.fr/assets/uploads/salon/nouvelleviepro-choisir_coaching.jpg', profileImage: 'https://www.computerhope.com/jargon/g/guest-user.jpg' };
+  coachInfo = { backgroundImage: 'https://www.nouvelleviepro.fr/assets/uploads/salon/nouvelleviepro-choisir_coaching.jpg', profileImage: 'https://www.computerhope.com/jargon/g/guest-user.jpg' };
   pageIndex = 'photo';
   readOnlyAproposB = true;
   user$: any = [];
+  showMore = false;
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @ViewChild('adresseinput') myInputField: ElementRef;
+  
   // eslint-disable-next-line max-len
-  info = { palmares: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod', pays: 'Tunis', ville: 'Ariana', codePostal: 2000, school: 'ESPRIT', lieuTravaille: 'La Marsa', dateN: new Date('01/01/1990') };
+  info = { palmares: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', pays: 'Tunis', ville: 'Ariana', codePostal: 2000, school: 'ESPRIT', lieuTravaille: 'La Marsa', dateN: new Date('01/01/1990') };
 
 
 
@@ -27,8 +32,13 @@ export class PalmaresPage implements OnInit {
     await this.getMe();
   }
 
-  readOnlyAproposToggle() {
+  readOnlyPalmaresToggle() {
     this.readOnlyAproposB = !this.readOnlyAproposB;
+  }
+  readOnlyAproposToggle() {
+
+    this.readOnlyAproposB = !this.readOnlyAproposB;
+    this.myInputField.nativeElement.focus();
   }
   async situationAm() {
     const alert = await this.alertCtrl.create({
@@ -59,7 +69,11 @@ export class PalmaresPage implements OnInit {
     await alert.present();
   }
 
-
+  trimString(string, length) {
+    return string.length > length ? 
+           string.substring(0, length) + '...' :
+           string;
+}
 
   getMe() {
     this.userService.getMe().subscribe(async res => {
@@ -74,7 +88,7 @@ export class PalmaresPage implements OnInit {
 
   updateCoach(){
     this.userService.updateUser(this.user$).subscribe(async res => {
-      console.log(this.user$);
+      console.log(res);
  
 
     });
