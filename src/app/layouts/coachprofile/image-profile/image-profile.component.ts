81dac1e1629f1fe91e34b822ee9d60ef8f047954
Achref <achref.yak@gmail.com>
2021-09-13 20:55:59 +0100

    home

--- src/app/layouts/coachprofile/image-profile/image-profile.component.ts
+++ src/app/layouts/coachprofile/image-profile/image-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { CameraSource } from '@capacitor/core';
+import { ActionSheetController, ModalController } from '@ionic/angular';
+import { ZipSubscriber } from 'rxjs/internal/observable/zip';
 import { environment } from 'src/environments/environment';
 import { ImageService } from '../../../shared/Service/image.service';
 @Component({
@@ -10,27 +12,69 @@ import { ImageService } from '../../../shared/Service/image.service';
 export class ImageProfileComponent implements OnInit {
   @Input() image: any;
   apImg = environment.apiImg + 'image/';
-  constructor(private modalController: ModalController, private imageService: ImageService) { }
+  constructor(
+    private modalController: ModalController,
+    private imageService: ImageService,
+    private action: ActionSheetController
+  ) {}
 
-  ngOnInit() { 
+  ngOnInit() {
     console.log(this.image);
   }
   dismiss() {
     // using the injected ModalController this page
     // can "dismiss" itself and optionally pass back data
     this.modalController.dismiss({
-      'dismissed': true
+      dismissed: true,
+    });
+  }
+  async addImage(source: CameraSource) {
+    console.log('addimage');
+
+    const fd = new FormData();
+    await this.imageService.readyImage(source, fd);
+    fd.append('createdBy', this.image.createdBy);
+    this.imageService.addImage(fd).subscribe(async (res) => {
+      console.log(res);
+    
     });
   }
 
+  async selectImageSource() {
+    const buttons = [
+      {
+        text: 'Take Photo',
+        icon: 'camera',
+        handler: () => {
+          this.addImage(CameraSource.Camera);
+        },
+      },
+      {
+        text: 'Choose from photos',
+        icon: 'image',
+        handler: () => {
+          this.addImage(CameraSource.Photos);
+        },
+      },
+    ];
+
+    const actionSheet = await this.action.create({
+      header: 'Select Image Source',
+      buttons,
+    });
+
+    await actionSheet.present();
+  }
   supprimer(image) {
-    this.imageService.deleteimage(image.id).subscribe(res => {
+    this.imageService.deleteimage(image.id).subscribe((res) => {
       console.log(res);
       this.dismiss();
-
-    })
+    });
   }
   modifier(image) {
-
+    this.imageService.updateImage(image).subscribe((res) => {
+      console.log(res);
+      this.dismiss();
+    });
   }
 }
