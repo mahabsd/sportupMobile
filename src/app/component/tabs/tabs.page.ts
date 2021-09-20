b3bbd5304d2130135d39433a69703c25b1ce6c40
mahabsd <mahabsd@gmail.com>
2021-09-20 17:14:27 +0100

    routing changes and add notifications

--- src/app/component/tabs/tabs.page.ts
+++ src/app/component/tabs/tabs.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CameraSource } from '@capacitor/core';
 import { ActionSheetController } from '@ionic/angular';
 import { ImageService } from 'src/app/Shared/Service/image.service';
+import { ModalController } from '@ionic/angular';
+import { ModalShearePage } from '../../layouts/home/modal-sheare/modal-sheare.page';
 
 @Component({
   selector: 'app-tabs',
@@ -9,19 +11,33 @@ import { ImageService } from 'src/app/Shared/Service/image.service';
   styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage implements OnInit {
+  user$: any = [];
+
   constructor(
     private imageService: ImageService,
-  ) {}
+    private modalController: ModalController,
 
-  ngOnInit() {}
+  ) { }
+
+  ngOnInit() { }
   sendMessage(message) {
     // send message to subscribers via observable subject
     this.imageService.sendMessage(message);
   }
 
-  
-
   add(event: any) {
     console.log(event);
+  }
+
+  async openModal() {
+    const modal = await this.modalController.create({
+      component: ModalShearePage,
+      componentProps: {
+        user: this.user$
+      }
+    });
+    await modal.present();
+    await modal.onWillDismiss().then((result) => {
+    });
   }
 }
