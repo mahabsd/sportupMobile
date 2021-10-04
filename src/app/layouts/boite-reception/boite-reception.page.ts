37165dc7be4ae56e7e6125bfdad6d8510cac4ea1
mahabsd <mahabsd@gmail.com>
2021-10-04 10:21:14 +0100

    fix menu profile

--- src/app/layouts/boite-reception/boite-reception.page.ts
+++ src/app/layouts/boite-reception/boite-reception.page.ts
@@ -22,7 +22,7 @@ export class BoiteReceptionPage implements OnInit {
 
   userid = '60f983fb06d9b3846c3d1030';
   isScrollTop: boolean;
-  constructor(private userservice: UserService, private followerService: FollowerService, private eventService: EventService,private chatService:ChatService
+  constructor(private userservice: UserService, private followerService: FollowerService, private eventService: EventService, private chatService: ChatService
   ) {
     this.addMoreItems();
   }
@@ -57,20 +57,19 @@ export class BoiteReceptionPage implements OnInit {
     this.userservice.getMe().subscribe(
       (response) => {
         this.chatService.getAllChatsByuser(response.data.data.id).subscribe((res) => {
-              this.users=res;
-              this.users.forEach(element => {
-                console.log(element.userReceiver)
-                this.userservice.getUser( element.userReceiver).subscribe(
-                  (response) => {
-                    this.users2.push(response.data.data)
-                    console.log(   response.data.data.role);
-
-                  },
-                  (error) => {
-                    console.error(error);
-                  }
-                ); 
-              });
+          this.users = res;
+          this.users.forEach(element => {
+            console.log(element.userReceiver)
+            this.userservice.getUser(element.userReceiver).subscribe(
+              (response) => {
+                this.users2.push(response.data.data)
+                console.log(response.data.data.role);
+              },
+              (error) => {
+                console.error(error);
+              }
+            );
+          });
           if (event) {
             event.target.complete()
           }
@@ -84,7 +83,7 @@ export class BoiteReceptionPage implements OnInit {
 
   }
 
-  
+
 
   loadData(event) {
 
@@ -94,7 +93,7 @@ export class BoiteReceptionPage implements OnInit {
       this.getfollow();
       this.numTimesLeft -= 1;
       event.target.complete();
-      this.users2=[]
+      this.users2 = []
 
     }, 500);
   }
