32b7729f6cebd903d73780a8f8ed2254095d9019
mahabsd <mahabsd@gmail.com>
2021-10-26 18:22:18 +0100

    add in chat search

--- src/app/component/modal/show-image/show-image.page.ts
+++ src/app/component/modal/show-image/show-image.page.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ModalController, NavParams, IonSlides } from '@ionic/angular';
 import { UserService } from 'src/app/Shared/Service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-show-image',
@@ -13,28 +14,44 @@ export class ShowImagePage implements OnInit {
   isItemAvailable: boolean;
   usersfiltered: any;
   userRole: any;
+  userConnectedId: any;
   constructor(
     private navParams: NavParams,
     private modalController: ModalController,
-    private userService: UserService
+    private userService: UserService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.getMe();
-
-
   }
   close() {
     this.modalController.dismiss();
   }
   getItems($event) {
+console.log(this.users);
 
     const val = $event.target.value;
     if (val && val.trim() !== '') {
       this.isItemAvailable = true;
-      this.usersfiltered = this.users.filter((item) => {
-        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1)
-      });
+      if (this.userRole === 'user' || this.userRole === 'pro'  ) {
+        this.userService.getRoleUsers().subscribe(res =>  {
+          this.users = res;
+          this.usersfiltered = this.users.filter((item) => {
+            return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1)
+          });
+         }
+          );
+      } else {
+        this.userService.getUsersKids().subscribe(res => {
+          this.users = res;
+          this.usersfiltered = this.users.filter((item) => {
+            return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1)
+          });
+        }
+          );
+      }
+
 
     } else {
       this.isItemAvailable = false;
@@ -42,12 +59,26 @@ export class ShowImagePage implements OnInit {
   }
   getMe() {
     this.userService.getMe().subscribe((res) => {
-      this.userRole= res.data.data.role;
-      if(this.userRole === 'user')  {
-        this.userService.getRoleUsers().subscribe(res => this.users = res);
-      }else {
-        this.userService.getUsersKids().subscribe(res => this.users = res);
-      }
+      this.userConnectedId = res.data.data._id
+      this.userRole = res.data.data.role;
     });
-}
+  }
+  gotoProfile(userId) {
+    if (userId === this.userConnectedId) {
+     if(this.userRole === 'user' || this.userRole === 'pro' ){
+      this.router.navigate(['menu/tabs/layouts/coachprofile/',userId,'me','coachphoto']);
+     } else  {
+// kids redirect
+     }
+      this.close();
+    } else {
+      if(this.userRole === 'user' || this.userRole === 'pro' ){
+        this.router.navigate(['menu/tabs/layouts/coachprofile',userId,'followed','coachphoto']);
+      }else{
+// kids redirect
+      }
+      this.close();
+    }
+  }
+
 }
