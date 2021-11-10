0599bbc228b817c6a34772a593bc5ee212b0053f
marouane <dhambrimarouane@gmail.com>
2021-11-10 12:43:37 +0100

    went back to calendar kids

--- src/app/layouts/kids/planning/planning.page.ts
+++ src/app/layouts/kids/planning/planning.page.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {  IonSlides } from '@ionic/angular';
 @Component({
   selector: 'app-planning',
   templateUrl: './planning.page.html',
   styleUrls: ['./planning.page.scss'],
 })
 export class PlanningPage implements OnInit {
+  @ViewChild(IonSlides) slides: IonSlides;
   events = [
-      {day: "lundi",
+    {day: "Lundi",
       date: "5",
       month: "Avril",
       year: "2021",
@@ -72,8 +73,97 @@ export class PlanningPage implements OnInit {
           text:"eat lunch"
         }
       ]
+    },
+    {day: "Jeudi",
+      date: "8",
+      month: "Avril",
+      year: "2021",
+      events: [
+        {
+          from: 8,
+          to: 9,
+          text:"cours de français"
+        },
+        {
+          from: 10,
+          to: 11,
+          text:"dance kids"
+        },
+        {
+          from: 12,
+          to: 14,
+          text:"eat lunch"
+        }
+      ]
+    },
+    {day: "Vendredi",
+      date: "9",
+      month: "Avril",
+      year: "2021",
+      events: [
+        {
+          from: 8,
+          to: 9,
+          text:"cours de français"
+        },
+        {
+          from: 10,
+          to: 11,
+          text:"dance kids"
+        },
+        {
+          from: 12,
+          to: 14,
+          text:"eat lunch"
+        }
+      ]
+    },
+    {day: "Samedi",
+      date: "10",
+      month: "Avril",
+      year: "2021",
+      events: [
+        {
+          from: 8,
+          to: 9,
+          text:"cours de français"
+        },
+        {
+          from: 10,
+          to: 11,
+          text:"dance kids"
+        },
+        {
+          from: 12,
+          to: 14,
+          text:"eat lunch"
+        }
+      ]
+    },
+    {day: "Dimanche",
+      date: "11",
+      month: "Avril",
+      year: "2021",
+      events: [
+        {
+          from: 8,
+          to: 9,
+          text:"cours de français"
+        },
+        {
+          from: 10,
+          to: 11,
+          text:"dance kids"
+        },
+        {
+          from: 12,
+          to: 14,
+          text:"eat lunch"
+        }
+      ]
     }
-    ]
+  ]
+    months=["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Septembre","Octobre","Novembre","Décembre"]
   ;
   constructor() { }
   checkIfNavDisabled(object, slideView) {
@@ -93,7 +183,7 @@ export class PlanningPage implements OnInit {
   }
 
   slideOptsTwo = {
-    initialSlide: 2,
+    initialSlide: this.events.length-1,
     spaceBetween: -50,
     slidesPerView: 3,
     centeredSlides: false,
@@ -162,7 +252,7 @@ export class PlanningPage implements OnInit {
 
           $slideEl.transform(slideTransform);
           $slideEl[0].style.zIndex =
-            -Math.abs(Math.round(offsetMultiplier)) + 1;
+            -Math.abs(Math.round(offsetMultiplier)) ;
           if (params.slideShadows) {
             // Set shadows
             let $shadowBeforeEl = isHorizontal
@@ -206,7 +296,103 @@ export class PlanningPage implements OnInit {
 
     },
   };
-  isScrollTop: boolean;
+
+  slideOptsOne = {
+    slidesPerView: 1,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    on: {
+      beforeInit() {
+        const swiper = this;
+
+        swiper.classNames.push(`${swiper.params.containerModifierClass}coverflow`);
+        swiper.classNames.push(`${swiper.params.containerModifierClass}3d`);
+
+        swiper.params.watchSlidesProgress = true;
+        swiper.originalParams.watchSlidesProgress = true;
+      },
+      setTranslate() {
+        const swiper = this;
+        const {
+          width: swiperWidth, height: swiperHeight, slides, $wrapperEl, slidesSizesGrid, $
+        } = swiper;
+        const params = swiper.params.coverflowEffect;
+        const isHorizontal = swiper.isHorizontal();
+        const transform$$1 = swiper.translate;
+        const center = isHorizontal ? -transform$$1 + (swiperWidth / 2) : -transform$$1 + (swiperHeight / 2);
+        const rotate = isHorizontal ? params.rotate : -params.rotate;
+        const translate = params.depth;
+        // Each slide offset from center
+        for (let i = 0, length = slides.length; i < length; i += 1) {
+          const $slideEl = slides.eq(i);
+          const slideSize = slidesSizesGrid[i];
+          const slideOffset = $slideEl[0].swiperSlideOffset;
+          const offsetMultiplier = ((center - slideOffset - (slideSize / 2)) / slideSize) * params.modifier;
+
+           let rotateY = isHorizontal ? rotate * offsetMultiplier : 0;
+          let rotateX = isHorizontal ? 0 : rotate * offsetMultiplier;
+          // var rotateZ = 0
+          let translateZ = -translate * Math.abs(offsetMultiplier);
+
+           let translateY = isHorizontal ? 0 : params.stretch * (offsetMultiplier);
+          let translateX = isHorizontal ? params.stretch * (offsetMultiplier) : 0;
+
+           // Fix for ultra small values
+          if (Math.abs(translateX) < 0.001) translateX = 0;
+          if (Math.abs(translateY) < 0.001) translateY = 0;
+          if (Math.abs(translateZ) < 0.001) translateZ = 0;
+          if (Math.abs(rotateY) < 0.001) rotateY = 0;
+          if (Math.abs(rotateX) < 0.001) rotateX = 0;
+
+           const slideTransform = `translate3d(${translateX}px,${translateY}px,${translateZ}px)  rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+           $slideEl.transform(slideTransform);
+          $slideEl[0].style.zIndex = -Math.abs(Math.round(offsetMultiplier)) + 1;
+          if (params.slideShadows) {
+            // Set shadows
+            let $shadowBeforeEl = isHorizontal ? $slideEl.find('.swiper-slide-shadow-left') : $slideEl.find('.swiper-slide-shadow-top');
+            let $shadowAfterEl = isHorizontal ? $slideEl.find('.swiper-slide-shadow-right') : $slideEl.find('.swiper-slide-shadow-bottom');
+            if ($shadowBeforeEl.length === 0) {
+              $shadowBeforeEl = swiper.$(`<div class="swiper-slide-shadow-${isHorizontal ? 'left' : 'top'}"></div>`);
+              $slideEl.append($shadowBeforeEl);
+            }
+            if ($shadowAfterEl.length === 0) {
+              $shadowAfterEl = swiper.$(`<div class="swiper-slide-shadow-${isHorizontal ? 'right' : 'bottom'}"></div>`);
+              $slideEl.append($shadowAfterEl);
+            }
+            if ($shadowBeforeEl.length) $shadowBeforeEl[0].style.opacity = offsetMultiplier > 0 ? offsetMultiplier : 0;
+            if ($shadowAfterEl.length) $shadowAfterEl[0].style.opacity = (-offsetMultiplier) > 0 ? -offsetMultiplier : 0;
+          }
+        }
+
+         // Set correct perspective for IE10
+        if (swiper.support.pointerEvents || swiper.support.prefixedPointerEvents) {
+          const ws = $wrapperEl[0].style;
+          ws.perspectiveOrigin = `${center}px 50%`;
+        }
+      },
+      setTransition(duration) {
+        const swiper = this;
+        swiper.slides
+          .transition(duration)
+          .find('.swiper-slide-shadow-top, .swiper-slide-shadow-right, .swiper-slide-shadow-bottom, .swiper-slide-shadow-left')
+          .transition(duration);
+      }
+    }
+  }
+
+  next(){
+    this.slides.slideNext();
+  }
+
+  prev(){
+    this.slides.slidePrev();
+  }
 
   ngOnInit() {
   }
