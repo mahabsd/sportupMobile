dc10eacb9769cbb400267033d3f02b163bd3ce0e
marouane <dhambrimarouane@gmail.com>
2021-11-11 07:43:36 +0100

    nearly finished notifications

--- src/app/layouts/notifications/notification/notification.component.ts
+++ src/app/layouts/notifications/notification/notification.component.ts
@@ -3,6 +3,7 @@ import { forkJoin } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { CommentService } from '../../../Shared/Service/comment.service';
 import { PostService } from '../../../shared/Service/post.service';
+import { UserService } from '../../../Shared/Service/user.service';
 
 @Component({
   selector: 'app-notification',
@@ -14,22 +15,28 @@ export class NotificationComponent implements OnInit {
   comments: any = [];
   images: any = [];
   mediafiles: any = [];
+  user: any = [];
   apiImgUser = `${environment.apiImg}User/`;
   apiImg = `${environment.apiImg}Post/`;
   constructor(private postService: PostService,
-    private commentService: CommentService,) { }
+    private commentService: CommentService,private userService: UserService,)
+    { }
     getMe() {
       this.getCommentByPost()
     }
+
   getCommentByPost() {
     forkJoin({
       comments: this.commentService.getCommentByService(this.notif.postId._id),
       images: this.postService.getPost(this.notif.postId._id),
       mediafiles: this.postService.getPost(this.notif.postId._id),
-    }).subscribe(({ comments, images, mediafiles }) => {
+      user: this.userService.getUser(this.notif.userOwner?.id),
+    }).subscribe(({ comments, images, mediafiles,user }) => {
       this.comments = comments;
       this.images = images.images;
       this.mediafiles = mediafiles.mediafiles;
+      this.user = user.data.data
+      console.log(this.user);
     });
   }
   getExt(fileName) {
