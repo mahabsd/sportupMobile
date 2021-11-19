b83b5ef591e6195d210d876bb5081cc59b60683a
marouane <dhambrimarouane@gmail.com>
2021-11-19 07:47:54 +0100

    finished design of events page

--- src/app/layouts/community/show-event/show-event.page.ts
+++ src/app/layouts/community/show-event/show-event.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { EventService } from 'src/app/shared/Service/event.service';
-import { ShowmorePage } from '../showmore/showmore.page';
 
 @Component({
   selector: 'app-show-event',
@@ -9,42 +8,25 @@ import { ShowmorePage } from '../showmore/showmore.page';
   styleUrls: ['./show-event.page.scss'],
 })
 export class ShowEventPage implements OnInit {
-  interrested;
+  interrested: boolean;
   isScrollTop: boolean;
   constructor(public popoverController: PopoverController,
-     private eventService: EventService) { }
+  private eventService: EventService) { }
 
   ngOnInit() {
-
   }
-
-
 
   interest() {
     this.interrested = true;
   }
+
   notinterested() {
     this.interrested = false;
   }
-  async openEventMenu(ev: any) {
-    const popover = await this.popoverController.create({
-      component: ShowmorePage,
-      cssClass: 'custClass',
-      event: ev,
-      translucent: true,
-      mode: 'ios',
-      componentProps: {
-        paramID: ev,
-      }
-    });
-    await popover.present();
 
-    const { role } = await popover.onDidDismiss();
-  }
   logScrolling(event) {
     if (event.detail.deltaY < 0) {
       this.isScrollTop = false;
-
     } else {
       this.isScrollTop = true;;
     }
