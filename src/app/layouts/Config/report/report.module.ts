b7e173c86df0b692143a96f34cc96377a6160a14
marouane <dhambrimarouane@gmail.com>
2022-01-07 08:55:54 +0100

    started page 'signaler'

---
+++ src/app/layouts/Config/report/report.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ReportPageRoutingModule } from './report-routing.module';
+
+import { ReportPage } from './report.page';
+import { ComponentModule } from '../../../component/component.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReportPageRoutingModule,
+    ComponentModule
+  ],
+  declarations: [ReportPage]
+})
+export class ReportPageModule { }
